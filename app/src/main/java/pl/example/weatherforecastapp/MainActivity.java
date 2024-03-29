66b3a01f16f8d0f95132d7c946812b4f3f83678f
Luckownia <f.kubala@interia.pl>
2024-03-29 19:01:58 +0100

    Configure connection with weather API part I

--- app/src/main/java/pl/example/weatherforecastapp/MainActivity.java
+++ app/src/main/java/pl/example/weatherforecastapp/MainActivity.java
@@ -1,6 +1,10 @@
 package pl.example.weatherforecastapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputLayout;
 
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    TextInputLayout textInputLayout;
+    TextView textView;
+    private final String url = "https://api.openweathermap.org/data/3.0/onecall?lat=";
+    private final String appid = "6b19c6b85668b0aa881c3d9a392fcbf8";
+    DecimalFormat df = new DecimalFormat("#.##");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +38,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textInputLayout = findViewById(R.id.textInputLayout);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void getWeather(View view) {
+        String tempUrl = "";
+        String city = textView.getText().toString().trim();
+        if(city.equals("")){
+            textView.setText("City field must be filled!!!");
+        }else{
+            tempUrl = url + city + "&appid=" +appid;
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String s) {
+                    Log.d("response",s);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    Toast.makeText(getApplicationContext(), volleyError.toString().trim(),Toast.LENGTH_SHORT).show();
+                }
+            });
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
     }
 }
