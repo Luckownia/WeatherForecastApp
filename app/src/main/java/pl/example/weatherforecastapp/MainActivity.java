1649775527543865ad64ad58a5138e5036b9dd90
Luckownia <f.kubala@interia.pl>
2024-03-30 18:15:37 +0100

    Complete API configuriation through JSON,changes in design concept

--- app/src/main/java/pl/example/weatherforecastapp/MainActivity.java
+++ app/src/main/java/pl/example/weatherforecastapp/MainActivity.java
@@ -3,6 +3,7 @@ package pl.example.weatherforecastapp;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,13 +21,18 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-    TextInputLayout textInputLayout;
+    EditText textInputLayout;
     TextView textView;
-    private final String url = "http://api.openweathermap.org/geo/1.0/direct?q=";
+    private final String url = "http://api.openweathermap.org/geo/1.0/direct?";
     private final String appid = "6b19c6b85668b0aa881c3d9a392fcbf8";
+    double lat,lon;
     DecimalFormat df = new DecimalFormat("#.##");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,23 +44,84 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        textInputLayout = findViewById(R.id.textInputLayout);
+        textInputLayout = findViewById(R.id.editText1);
         textView = findViewById(R.id.textView);
     }
 
     public void getWeather(View view) {
         String tempUrl = "";
-        String city = textInputLayout.getEditText().toString().trim();
-        System.out.println("Miastooooooo");
-        System.out.println(city);
+        String city = textInputLayout.getText().toString().trim();
+
         if(city.equals("")){
             textView.setText("City field must be filled!!!");
         }else{
-            tempUrl = url + city + "&appid=" +appid;
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+            tempUrl = url +"q="+ city + "&appid=" +appid;
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, tempUrl, new Response.Listener<String>() {
                 @Override
-                public void onResponse(String s) {
-                    Log.d("response",s);
+                public void onResponse(String response) {
+                    Log.d("response",response);
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        if (jsonArray.length() > 0) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(0);
+                            lat = jsonObject.getDouble("lat");
+                            lon = jsonObject.getDouble("lon");
+                            System.out.println(lat);
+                            System.out.println(lon);
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    Toast.makeText(getApplicationContext(), volleyError.toString().trim(),Toast.LENGTH_SHORT).show();
+                }
+            });
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
+        if(true) //Trzeba dwukrotnie zrobic jsona jak to zrobic bardziej wydajnie czy dać  to wczesniej?
+        {
+
+            String tempUrlLoc = "https://api.openweathermap.org/data/2.5/weather?";
+            tempUrlLoc+="lat="+lat+"&lon="+lon+"&appid="+appid;
+            System.out.println(tempUrlLoc);
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, tempUrlLoc, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response2) {
+                    Log.d("response",response2);
+                    String output = "";
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response2);
+                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        String description = jsonObjectWeather.getString("description");
+                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                        double temp = jsonObjectMain.getDouble("temp")-273.15;
+                        double feelsLike = jsonObjectMain.getDouble("feels_like")-273.15;
+                        float pressure = jsonObjectMain.getInt("pressure");
+                        int humidity = jsonObjectMain.getInt("humidity");
+                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                        float speedWind = jsonObjectWind.getInt("speed");
+                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                        int cloudy = jsonObjectClouds.getInt("all"); //cloudiness percantage
+                        //String cityName = jsonResponse.getString("name"); Problemy z pobraniem city z json-a
+                        //System.out.println(description+ " "+ temp+" "+pressure+ " "+ humidity+ " "+ speedWind+ " "+cloudy+" "+city);
+                        output = "City="+city+"\n"
+                                +"description="+description+"\n"
+                                +"tempreature="+ temp+"\n "
+                                +"feels like="+ feelsLike+"\n "
+                                +"Pressure="+pressure+"\n"
+                                +"Humidity="+humidity+"\n"
+                                +"Speed wind="+speedWind+"\n"
+                                +"Cloudy="+cloudy+"\n";
+                        textView.setText(output);
+
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }, new Response.ErrorListener() {
                 @Override
