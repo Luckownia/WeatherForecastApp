573b112a2430d6803620c9718aa6793746c5ec39
Fresh <kizzak1111@gmail.com>
2024-04-04 19:03:28 +0200

    Wyswietlenie informacji kiedy wpisane miasto nie istnieje lub nie wpisano nic, trzeba kliknac w guzik zeby rozpoczac wyszkiwanie, wystarczy teraz kliknac raz zeby wyszukalo (trzeba bylo przeniesc to drugie zadanie do response)

--- app/src/main/java/pl/example/weatherforecastapp/MainActivity.java
+++ app/src/main/java/pl/example/weatherforecastapp/MainActivity.java
@@ -32,8 +32,9 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     private final String url = "http://api.openweathermap.org/geo/1.0/direct?";
     private final String appid = "6b19c6b85668b0aa881c3d9a392fcbf8";
-    double lat,lon;
+    double lat, lon;
     DecimalFormat df = new DecimalFormat("#.##");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,14 +53,14 @@ public class MainActivity extends AppCompatActivity {
         String tempUrl = "";
         String city = textInputLayout.getText().toString().trim();
 
-        if(city.equals("")){
+        if (city.equals("")) {
             textView.setText("City field must be filled!!!");
-        }else{
-            tempUrl = url +"q="+ city + "&appid=" +appid;
+        } else {
+            tempUrl = url + "q=" + city + "&appid=" + appid;
             StringRequest stringRequest = new StringRequest(Request.Method.GET, tempUrl, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
-                    Log.d("response",response);
+                    Log.d("response", response);
                     try {
                         JSONArray jsonArray = new JSONArray(response);
                         if (jsonArray.length() > 0) {
@@ -68,6 +69,53 @@ public class MainActivity extends AppCompatActivity {
                             lon = jsonObject.getDouble("lon");
                             System.out.println(lat);
                             System.out.println(lon);
+
+                            // Drugie żądanie sieciowe
+                            String tempUrlLoc = "https://api.openweathermap.org/data/2.5/weather?";
+                            tempUrlLoc += "lat=" + lat + "&lon=" + lon + "&appid=" + appid;
+                            System.out.println(tempUrlLoc);
+                            StringRequest stringRequest2 = new StringRequest(Request.Method.GET, tempUrlLoc, new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response2) {
+                                    Log.d("response", response2);
+                                    String output = "";
+                                    try {
+                                        JSONObject jsonResponse = new JSONObject(response2);
+                                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                                        String description = jsonObjectWeather.getString("description");
+                                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                                        double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                                        double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                                        float pressure = jsonObjectMain.getInt("pressure");
+                                        int humidity = jsonObjectMain.getInt("humidity");
+                                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                                        float speedWind = jsonObjectWind.getInt("speed");
+                                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                                        int cloudy = jsonObjectClouds.getInt("all");
+                                        output = "City=" + city + "\n"
+                                                + "description=" + description + "\n"
+                                                + "temperature=" + temp + "\n "
+                                                + "feels like=" + feelsLike + "\n "
+                                                + "Pressure=" + pressure + "\n"
+                                                + "Humidity=" + humidity + "\n"
+                                                + "Speed wind=" + speedWind + "\n"
+                                                + "Cloudy=" + cloudy + "\n";
+                                        textView.setText(output);
+                                    } catch (JSONException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError volleyError) {
+                                    Toast.makeText(getApplicationContext(), volleyError.toString().trim(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            RequestQueue requestQueue2 = Volley.newRequestQueue(getApplicationContext());
+                            requestQueue2.add(stringRequest2);
+                        } else{
+                            textView.setText("City not found.");
                         }
                     } catch (JSONException e) {
                         throw new RuntimeException(e);
@@ -76,57 +124,7 @@ public class MainActivity extends AppCompatActivity {
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError volleyError) {
-                    Toast.makeText(getApplicationContext(), volleyError.toString().trim(),Toast.LENGTH_SHORT).show();
-                }
-            });
-            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-            requestQueue.add(stringRequest);
-        }
-        if(true) //Trzeba dwukrotnie zrobic jsona jak to zrobic bardziej wydajnie czy dać  to wczesniej?
-        {
-
-            String tempUrlLoc = "https://api.openweathermap.org/data/2.5/weather?";
-            tempUrlLoc+="lat="+lat+"&lon="+lon+"&appid="+appid;
-            System.out.println(tempUrlLoc);
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, tempUrlLoc, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response2) {
-                    Log.d("response",response2);
-                    String output = "";
-                    try {
-                        JSONObject jsonResponse = new JSONObject(response2);
-                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
-                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
-                        String description = jsonObjectWeather.getString("description");
-                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
-                        double temp = jsonObjectMain.getDouble("temp")-273.15;
-                        double feelsLike = jsonObjectMain.getDouble("feels_like")-273.15;
-                        float pressure = jsonObjectMain.getInt("pressure");
-                        int humidity = jsonObjectMain.getInt("humidity");
-                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
-                        float speedWind = jsonObjectWind.getInt("speed");
-                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
-                        int cloudy = jsonObjectClouds.getInt("all"); //cloudiness percantage
-                        //String cityName = jsonResponse.getString("name"); Problemy z pobraniem city z json-a
-                        //System.out.println(description+ " "+ temp+" "+pressure+ " "+ humidity+ " "+ speedWind+ " "+cloudy+" "+city);
-                        output = "City="+city+"\n"
-                                +"description="+description+"\n"
-                                +"tempreature="+ temp+"\n "
-                                +"feels like="+ feelsLike+"\n "
-                                +"Pressure="+pressure+"\n"
-                                +"Humidity="+humidity+"\n"
-                                +"Speed wind="+speedWind+"\n"
-                                +"Cloudy="+cloudy+"\n";
-                        textView.setText(output);
-
-                    } catch (JSONException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError volleyError) {
-                    Toast.makeText(getApplicationContext(), volleyError.toString().trim(),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), volleyError.toString().trim(), Toast.LENGTH_SHORT).show();
                 }
             });
             RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
